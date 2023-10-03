af839293f164f3bb219dde258f97a6cfc58c5735
Self-Lakshh <lakshyachopra2004@gmail.com>
2023-10-03 13:15:00 +0530

    add basic script functions for dom events

--- Day-19/script.js
+++ Day-19/script.js
@@ -1,1 +1,8 @@
-// DOM manipulation practice
+// Get references to elements
+const heading = document.getElementById("heading");
+const btn = document.getElementById("btn");
+
+// Add click listener
+btn.addEventListener("click", () => {
+    heading.textContent = "You clicked the button!";
+});
