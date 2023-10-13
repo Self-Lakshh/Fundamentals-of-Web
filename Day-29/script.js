e561ca8bcd558ee68bed13e55d3d29f34f3024ea
Self-Lakshh <lakshyachopra2004@gmail.com>
2023-10-13 13:15:00 +0530

    implement project details data array

---
+++ Day-29/script.js
@@ -0,0 +1,8 @@
+// reusable function for rendering project cards dynamically
+const projects = [
+    { title: "HTML Form", desc: "Form elements and fields setup", cat: "html" },
+    { title: "Grid Photo Gallery", desc: "Responsive CSS grid gallery layout", cat: "css" },
+    { title: "Todo Application", desc: "Cached Todo task lists app", cat: "javascript" }
+];
+
+console.log("Portfolio Interactions Loaded!");
